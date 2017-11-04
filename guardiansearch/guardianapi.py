08e8ee6b183da7b1bb5873288ced43e331d8be72
brendand <brendan.j.donegan@gmail.com>
2017-11-04 19:00:24 +0000

    Finish more of the tests and add some code to make a few of them pass

--- guardiansearch/guardianapi.py
+++ guardiansearch/guardianapi.py
@@ -1,1 +1,26 @@
+import requests
 
+
+GUARDIAN_API = "http://content.guardianapis.com/search"
+
+
+def get_result_json(api_key, query=None, from_date=None, to_date=None, page=None):
+    # Assemble the parameters of the request
+    params = {"api-key": api_key}
+    if query is not None:
+        params['q'] = query
+    if from_date is not None:
+        params['from_date'] = from_date
+    if to_date is not None:
+        params['to_date'] = to_date
+    if page is not None:
+        params['page'] = page
+    response = requests.get(GUARDIAN_API, params=params)
+    if response.ok:
+        return response.json()
+    # If the request fails return the error message
+    return response.text
+
+
+def format_results(results):
+    return [r['webTitle'] + " - " + r["webUrl"] for r in results]
--- guardiansearch/tests/test_guardian_search.py
+++ guardiansearch/tests/test_guardian_search.py
@@ -1,3 +1,5 @@
+import mock
+import os
 import unittest
 
 from .. import guardianapi
@@ -7,32 +9,56 @@ class TestGuardianSearch(unittest.TestCase):
     def test_get_result_json(self):
         # Verify that the correct API call is made and result JSON returned
         # Have to mock the result of requests.get
-        expected_json = {}
-        result_json = guardianapi.get_result_json(
-            query="foo bar",
-            from_date="2012-06-06",
-            to_date="2014-06-06",
-            page=2,
-        )
-        # Assert that requests.get is called correctly
-        self.assertEqual(expected_json, result_json)
+        expected_json = {
+            'response': {
+                'status': 'ok',
+            }
+        }
+        with mock.patch('requests.get') as requests_get:
+            requests_get.return_value.ok = True
+            requests_get.return_value.json.return_value = expected_json
+            result_json = guardianapi.get_result_json(
+                api_key=os.environ['API_KEY'],
+                query="foo bar",
+                from_date="2012-06-06",
+                to_date="2014-06-06",
+                page=2,
+            )
+            # Assert that requests.get is called correctly
+            requests_get.assert_called_once_with(
+                guardianapi.GUARDIAN_API,
+                params={
+                    'api-key': os.environ['API_KEY'],
+                    'q': "foo bar",
+                    'from_date': "2012-06-06",
+                    'to_date': "2014-06-06",
+                    'page': 2,
+                }
+            )
+            self.assertEqual(expected_json, result_json)
 
     def test_get_result_json_not_ok(self):
         # Have to mock the result of requests.get
         expected_json = {}
         result_json = guardianapi.get_result_json(
+            api_key=os.environ['API_KEY'],
             query="foo bar",
         )
+        self.assertEqual(expected_json, result_json)
 
     def test_format_results(self):
         # Verify that results are formatted correctly
-        expected_formatted = ""
-        formatted_results = guardianapi.format_results({})
+        expected_formatted = ["result 1 - http://result1.com"]
+        formatted_results = guardianapi.format_results(
+            [
+                {'webTitle': 'result 1', 'webUrl': 'http://result1.com'}
+            ]
+        )
         self.assertEqual(expected_formatted, formatted_results)
 
     def test_get_search_results(self):
         # Verify that if there are multiple pages all results are returned
-        expected_results = ""
+        expected_results = ["result 1 - http://result1.com"]
         received_results = guardianapi.get_search_results(
             query="foo bar",
             from_date="2012-06-06",
